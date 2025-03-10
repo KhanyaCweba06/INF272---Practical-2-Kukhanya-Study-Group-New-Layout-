e2c2b8b7a66af6506a9ac0369ba3163d07871432
Kukhanya Cweba <u21598569@tuks.co.za>
2025-03-11 00:13:38 +0200

    Add files via upload

---
+++ StudyGroupPeople.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace INF272___Practical_2__Kukhanya_Study_Group_New_Layout_.Models
+{
+    public class StudyGroupPeople
+    {
+        [Display(Name = "Student Number")]
+        public string StudentNumber { get; set; }
+
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Surname")]
+        public string Surname { get; set; }
+
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+        [Display(Name = "Link To Personal Page")]
+        public string MyLink { get; set; }
+    }
+}
---
+++ StudyGroupPeopleController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace INF272___Practical_2__Kukhanya_Study_Group_New_Layout_.Controllers
+{
+    public class StudyGroupPeopleController : Controller
+    {
+        // GET: StudyGroupPeople
+        public ActionResult People()
+        {
+            List<Models.StudyGroupPeople> People = new List<Models.StudyGroupPeople>();
+
+            People.Add(new Models.StudyGroupPeople { StudentNumber = "u21598569", Name = "Kukhanya", Surname = "Cweba", EmailAddress = "u21598569@tuks.co.za", MyLink = "~/HTML/Kukhanya.html" });
+            People.Add(new Models.StudyGroupPeople { StudentNumber = "u24765347", Name = "John", Surname = "Devereux", EmailAddress = "u24765347@tuks.co.za", MyLink = "~/HTML/John.html" });
+            People.Add(new Models.StudyGroupPeople { StudentNumber = "u22765432", Name = "Kevin", Surname = "Knox", EmailAddress = "u22765432@tuks.co.za", MyLink = "~/HTML/Kevin.html" });
+            People.Add(new Models.StudyGroupPeople { StudentNumber = "u19876543", Name = "Joyce", Surname = "Hendricks", EmailAddress = "u19876543@tuks.co.za", MyLink = "~/HTML/Joyce.html" });
+            People.Add(new Models.StudyGroupPeople { StudentNumber = "u23145676", Name = "Benjamin", Surname = "Fournier", EmailAddress = "u23145676@tuks.co.za", MyLink = "~/HTML/Benjamin.html" });
+
+            return View(People);
+        }
+    }
+}
